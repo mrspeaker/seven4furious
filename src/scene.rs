d9e577c898b742dd4c670bd1bc9aeb0e8e5e36cd
Mr Speaker <mrspeaker@gmail.com>
2024-03-07 04:29:51 +0800

    banana4scale

--- src/scene.rs
+++ src/scene.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use std::f32::consts::PI;
 
 pub struct ScenePlugin;
 
@@ -17,10 +18,19 @@ fn init_scene(
         color: Color::WHITE,
         brightness: 1000.0,
     });
+
     commands.spawn(SceneBundle {
         scene: assets.load("scene.gltf#Scene0"),
-        transform: Transform::from_xyz(0.,0.,-200.)
+        transform: Transform::from_xyz(0.,-3.0,-200.)
             .with_scale(Vec3::ONE * 2.0),
+        ..default()
+    });
+
+    commands.spawn(SceneBundle {
+        scene: assets.load("car.glb#Scene0"),
+        transform: Transform::from_xyz(0.,0.,-300.)
+            .with_scale(Vec3::ONE * 1.0)
+            .with_rotation(Quat::from_rotation_y(-PI/2.0)),
         ..default()
     });
 
